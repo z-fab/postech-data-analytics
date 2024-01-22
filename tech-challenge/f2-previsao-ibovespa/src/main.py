922e2a2b5221b168d53e8c9345b7438a58ad5855
Fabricio Zillig <fabricio.zillig@gmail.com>
2024-01-22 18:38:03 -0300

    Iniciando Fase 3 - Big Data

--- tech-challenge/f2-previsao-ibovespa/src/main.py
+++ tech-challenge/f2-previsao-ibovespa/src/main.py
@@ -14,28 +14,34 @@ from modules.models import Consolidate
 
 # Definindo variáveis de ambiente com os caminhos dos diretórios
 PATHS = {}
-PATHS['SRC_DIR'] = os.path.dirname(__file__)
-PATHS['BASE_DIR'] = os.path.dirname(PATHS['SRC_DIR'])
-PATHS['DATA_DIR'] = os.path.join(PATHS['BASE_DIR'], 'data')
-PATHS['LOG_DIR'] = os.path.join(PATHS['BASE_DIR'], 'log')
+PATHS["SRC_DIR"] = os.path.dirname(__file__)
+PATHS["BASE_DIR"] = os.path.dirname(PATHS["SRC_DIR"])
+PATHS["DATA_DIR"] = os.path.join(PATHS["BASE_DIR"], "data")
+PATHS["LOG_DIR"] = os.path.join(PATHS["BASE_DIR"], "log")
 
 for key, value in PATHS.items():
     os.environ[key] = value
 
 # Criando configuracao de logging
-logger = logging.getLogger('f2-log')
+logger = logging.getLogger("f2-log")
 logger.setLevel(logging.DEBUG)
 
 c_handler = logging.StreamHandler()
 c_handler.setLevel(logging.DEBUG)
-c_format = logging.Formatter('%(asctime)s :: (%(name)s | %(filename)s) %(levelname)s - %(message)s')
+c_format = logging.Formatter(
+    "%(asctime)s :: (%(name)s | %(filename)s) %(levelname)s - %(message)s"
+)
 c_handler.setFormatter(c_format)
 logger.addHandler(c_handler)
 
-f_handler = handlers.TimedRotatingFileHandler(f"{PATHS['LOG_DIR']}/f2.log", when="midnight", backupCount=15)
+f_handler = handlers.TimedRotatingFileHandler(
+    f"{PATHS['LOG_DIR']}/f2.log", when="midnight", backupCount=15
+)
 f_handler.suffix = "%Y%m%d"
 f_handler.setLevel(logging.INFO)
-f_format = logging.Formatter('%(asctime)s :: (%(name)s | %(filename)s) %(levelname)s - %(message)s')
+f_format = logging.Formatter(
+    "%(asctime)s :: (%(name)s | %(filename)s) %(levelname)s - %(message)s"
+)
 f_handler.setFormatter(f_format)
 logger.addHandler(f_handler)
 
@@ -43,7 +49,7 @@ logger.addHandler(f_handler)
 def ingestion():
     logger.info("\n[ INICIANDO PROCESSO DE INGESTÃO ]\n")
 
-    db = Database(path = os.path.join(os.getenv('DATA_DIR'), 'ibovespa.db'))
+    db = Database(path=os.path.join(os.getenv("DATA_DIR"), "ibovespa.db"))
 
     last_date = db.getLastDateIbov()
     yesterday = datetime.datetime.now().date() - datetime.timedelta(days=1)
@@ -51,25 +57,27 @@ def ingestion():
     # Se não houver dados no banco de dados, coletar a partir de 01/01/2000
     if last_date is None:
         last_date = datetime.date(2000, 1, 1)
-        logger.info(f'Não há dados no banco de dados, coletando a partir de {last_date}')
+        logger.info(
+            f"Não há dados no banco de dados, coletando a partir de {last_date}"
+        )
 
     # Se a última data no Banco for maior ou igual a ontem, não há necessidade de coletar novos dados
     elif last_date >= yesterday:
-        logger.info('Não há novos dados para serem coletados')
+        logger.info("Não há novos dados para serem coletados")
         return None
 
     # Se a última data no Banco for menor que ontem, coletar a partir do dia seguinte
     else:
         last_date = last_date + datetime.timedelta(days=1)
-        logger.info('Coletando dados a partir de %s', last_date)
+        logger.info("Coletando dados a partir de %s", last_date)
 
     data_ibovespa = BovespaCollector().get_data(start_date=last_date)
 
     # Se não houver dados para serem salvos no banco de dados, não há necessidade de salvar
     if data_ibovespa is None or len(data_ibovespa) <= 0:
-        logger.info('Nenhum dado para ser salvo no banco de dados')
+        logger.info("Nenhum dado para ser salvo no banco de dados")
         return None
-    
+
     # Salvando dados no banco de dados
     db.insertIbov(data_ibovespa)
 
@@ -79,38 +87,47 @@ def ingestion():
 
 
 def predict():
-
     logger.info("\n[ INICIANDO PROCESSO DE PREDIÇÃO ]\n")
 
-    db = Database(path = os.path.join(os.getenv('DATA_DIR'), 'ibovespa.db'))
-    
+    db = Database(path=os.path.join(os.getenv("DATA_DIR"), "ibovespa.db"))
+
     # Verificando a última data de predição. Caso não haja, iniciar a partir de 01/06/2023
     lastDatePredicted = db.getLastDatePred()
-    lastDatePredicted = lastDatePredicted if lastDatePredicted is not None else datetime.date(2022, 1, 1)
-    
+    lastDatePredicted = (
+        lastDatePredicted
+        if lastDatePredicted is not None
+        else datetime.date(2022, 1, 1)
+    )
+
     lastDateIbov = db.getLastDateIbov()
 
     # Se a última data de predição for maior que a última data que temos do Ibovespa, não há necessidade de novas predições
-    if lastDatePredicted > lastDateIbov:
-        logger.info('Última data de predição: %s. Não há necessidade de novas predições', lastDatePredicted)
+    if (lastDatePredicted > lastDateIbov) & (
+        lastDatePredicted >= datetime.datetime.now().date()
+    ):
+        logger.info(
+            "Última data de predição: %s. Não há necessidade de novas predições",
+            lastDatePredicted,
+        )
         return None
 
-    logger.info('Última data de predição: %s. Realizando predições!', lastDatePredicted)
+    logger.info("Última data de predição: %s. Realizando predições!", lastDatePredicted)
 
     # Coletando dados do Ibovespa para realizar a predição
     data_ibovespa = db.getDataIbov(
-        start_date=lastDatePredicted - datetime.timedelta(days=180), 
-        end_date=lastDatePredicted
+        start_date=lastDatePredicted - datetime.timedelta(days=180),
+        end_date=lastDatePredicted,
     )
 
     lastDateReturn = data_ibovespa[-1][0]
-    lastDateReturn = datetime.datetime.strptime(lastDateReturn, '%Y-%m-%d').date()
-    print(lastDateReturn >= lastDatePredicted)
+    lastDateReturn = datetime.datetime.strptime(lastDateReturn, "%Y-%m-%d").date()
 
     if lastDateReturn >= lastDatePredicted:
-        pred = Predictor(data_ibovespa).predict()
-    else:  
-        pred = Predictor(data_ibovespa, last_date=lastDatePredicted).predict()
+        pred = Predictor(data_ibovespa).predict(n_periods=1)
+    else:
+        pred = Predictor(data_ibovespa, last_date=lastDatePredicted).predict(
+            n_periods=1
+        )
 
     db.insertPredict(pred)
 
@@ -121,26 +138,25 @@ def predict():
     return None
 
 
-
 def consolidate():
-
     logger.info("\n[ INICIANDO PROCESSO DE CONSOLIDATE ]\n")
 
-    db = Database(path = os.path.join(os.getenv('DATA_DIR'), 'ibovespa.db'))
+    db = Database(path=os.path.join(os.getenv("DATA_DIR"), "ibovespa.db"))
     start_date = datetime.date(2023, 1, 1)
     end_date = db.getLastDatePred()
 
     # Se não houver dados de predição usaremos como data final a última data do Ibovespa
     if end_date is None:
         end_date = db.getLastDateIbov()
-        logger.info('Não há dados de predição. Usando Ibovespa como data final: %s', end_date)
+        logger.info(
+            "Não há dados de predição. Usando Ibovespa como data final: %s", end_date
+        )
 
     # Criando lista de objetos Consolidate
     list_consolidate = []
 
     # Loop para gerar os objetos Consolidate.
     while start_date <= end_date:
-
         pred = db.getDataPred(start_date=start_date, end_date=start_date)
         ibov = db.getDataIbov(start_date=start_date, end_date=start_date)
 
@@ -149,18 +165,14 @@ def consolidate():
             pred = 0
         else:
             pred = pred[0][-1]
-        
+
         if ibov is None:
             ibov = 0
         else:
             ibov = ibov[0][-1]
 
         # Criando objeto Consolidate
-        consolidate = Consolidate(
-            date = start_date,
-            close = ibov,
-            predict = pred
-        )
+        consolidate = Consolidate(date=start_date, close=ibov, predict=pred)
         list_consolidate.append(consolidate)
 
         # Incrementando data em 1 dia
@@ -172,19 +184,18 @@ def consolidate():
 
     return None
 
+
 def main():
-    
     ingestion()
     predict()
     consolidate()
 
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     main()
     schedule.every().day.at("07:00").do(main)
+    schedule.every().day.at("12:00").do(main)
 
     while True:
         schedule.run_pending()
         time.sleep(1)
-
--- tech-challenge/f2-previsao-ibovespa/src/modules/predict.py
+++ tech-challenge/f2-previsao-ibovespa/src/modules/predict.py
@@ -4,63 +4,64 @@ from pmdarima import auto_arima
 
 from .models import PredictIbovespa
 
-logger = logging.getLogger('f2-log')
+logger = logging.getLogger("f2-log")
+
 
 class Predictor:
-
-    def __init__(self, data_raw, last_date = None):
+    def __init__(self, data_raw, last_date=None):
         self.df = self._preprocessing(data_raw, last_date)
 
-
-    def _preprocessing(self, data, last_date = None):
+    def _preprocessing(self, data, last_date=None):
         logger.info("Iniciando pré-processamento dos dados")
 
         df = pd.DataFrame(data)
-        df = df[['date', 'close']]
-        df.rename(columns={'close': 'y', 'date': 'ds'}, inplace=True)
+        df = df[["date", "close"]]
+        df.rename(columns={"close": "y", "date": "ds"}, inplace=True)
 
         if last_date is not None:
-            pd_last_date = pd.DataFrame({'ds': [last_date], 'y': [None]})
+            pd_last_date = pd.DataFrame({"ds": [last_date], "y": [None]})
             df = pd.concat([df, pd_last_date], ignore_index=True, axis=0)
 
-        df['ds'] = pd.to_datetime(df['ds'])
-        df = df.sort_values(by=['ds'])
+        df["ds"] = pd.to_datetime(df["ds"])
+        df = df.sort_values(by=["ds"])
 
-        ds_min = df['ds'].min().date()
-        ds_max = df['ds'].max().date()
-        
-        df.set_index('ds', inplace=True)
-        df = df.asfreq('D')
+        ds_min = df["ds"].min().date()
+        ds_max = df["ds"].max().date()
+
+        df.set_index("ds", inplace=True)
+        df = df.asfreq("D")
         df = df.ffill()
 
-        logger.info("Pré-processamento finalizado. Total de registros: %s [Inicio %s : Fim %s]", len(df), ds_min, ds_max)
+        logger.info(
+            "Pré-processamento finalizado. Total de registros: %s [Inicio %s : Fim %s]",
+            len(df),
+            ds_min,
+            ds_max,
+        )
         return df
 
     def getDataFrame(self):
         return self.df
 
-    def predict(self, last_date = None):
+    def predict(self, n_periods=3):
         logger.info("Iniciando predição")
         model_arima = auto_arima(
             self.df,
             seasonal=True,
             trace=False,
-            error_action='ignore',
+            error_action="ignore",
             suppress_warnings=True,
         )
-        predict = model_arima.predict(n_periods=3)
-        
+        predict = model_arima.predict(n_periods=n_periods)
+
         list_predict = []
         for p in predict.reset_index().iterrows():
-
             predict = PredictIbovespa(
-                date=p[1]['index'].strftime('%Y-%m-%d'),
-                predict=p[1][0]
+                date=p[1]["index"].strftime("%Y-%m-%d"), predict=p[1][0]
             )
             logger.info("Objeto de predição criado: %s", predict.__dict__)
             list_predict.append(predict)
 
         logger.info("Predição finalizada.")
-
 
         return list_predict
