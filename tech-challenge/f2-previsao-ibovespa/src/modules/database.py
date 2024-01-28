19ee0ec575eb3ae79ecc19389b7511f4746ca0e9
Fabricio Zillig <fabricio.zillig@gmail.com>
2024-01-28 09:29:34 -0300

    Ajustes em alguns scripts do Tech Challenge da Fase 2

--- tech-challenge/f2-previsao-ibovespa/src/modules/database.py
+++ tech-challenge/f2-previsao-ibovespa/src/modules/database.py
@@ -6,18 +6,18 @@ import sqlalchemy as db
 from .models import IBovespaData
 from .models import PredictIbovespa
 
-logger = logging.getLogger('f2-log')
+logger = logging.getLogger("f2-log")
+
 
 class Database:
-
-    def __init__(self, path:str = None):
+    def __init__(self, path: str = None):
         if not path:
-            path = os.path.join(os.path.dirname(__file__), 'ibovespa.db')
-        self.engine = db.create_engine('sqlite:///'+path)
+            path = os.path.join(os.path.dirname(__file__), "ibovespa.db")
+        self.engine = db.create_engine("sqlite:///" + path)
         self._create_tables()
 
     def _create_tables(self):
-        query_ibovespa  = '''
+        query_ibovespa = """
             CREATE TABLE IF NOT EXISTS ibovespa (
                 date DATE PRIMARY KEY,
                 open FLOAT,
@@ -25,15 +25,15 @@ class Database:
                 low FLOAT,
                 close FLOAT
             );
-        '''
-        query_predict  = '''
+        """
+        query_predict = """
             CREATE TABLE IF NOT EXISTS predict (
                 date DATE PRIMARY KEY,
                 predict FLOAT
             );
-        '''
+        """
 
-        query_consolidate  = '''
+        query_consolidate = """
             CREATE TABLE IF NOT EXISTS consolidate (
                 date DATE PRIMARY KEY,
                 close FLOAT,
@@ -41,7 +41,7 @@ class Database:
                 diff FLOAT,
                 diff_percent FLOAT
             );
-        '''
+        """
 
         with self.engine.connect() as conn:
             conn.execute(db.text(query_ibovespa))
@@ -49,205 +49,244 @@ class Database:
             conn.execute(db.text(query_consolidate))
 
     def getLastDateIbov(self):
-        query = '''
+        query = """
             SELECT date FROM ibovespa ORDER BY date DESC LIMIT 1;
-        '''
+        """
 
         with self.engine.connect() as conn:
             result = conn.execute(db.text(query)).all()
-            logger.debug('Última data do Ibovespa no banco de dados: %s', result)
+            logger.debug("Última data do Ibovespa no banco de dados: %s", result)
             if len(result) <= 0:
                 return None
-            return datetime.datetime.strptime(result[0][0], '%Y-%m-%d').date()
+            return datetime.datetime.strptime(result[0][0], "%Y-%m-%d").date()
 
     def getLastDatePred(self):
-        query = '''
+        query = """
             SELECT date FROM predict ORDER BY date DESC LIMIT 1;
-        '''
+        """
 
         with self.engine.connect() as conn:
             result = conn.execute(db.text(query)).all()
-            logger.debug('Última data de predição no banco de dados: %s', result)
+            logger.debug("Última data de predição no banco de dados: %s", result)
             if len(result) <= 0:
                 return None
-            return datetime.datetime.strptime(result[0][0], '%Y-%m-%d').date()
+            return datetime.datetime.strptime(result[0][0], "%Y-%m-%d").date()
 
     def getLastDateConsolidate(self):
-        query = '''
+        query = """
             SELECT date FROM consolidate ORDER BY date DESC LIMIT 1;
-        '''
+        """
 
         with self.engine.connect() as conn:
             result = conn.execute(db.text(query)).all()
-            logger.debug('Última data consolidada no banco de dados: %s', result)
+            logger.debug("Última data consolidada no banco de dados: %s", result)
             if len(result) <= 0:
                 return None
-            return datetime.datetime.strptime(result[0][0], '%Y-%m-%d').date()
+            return datetime.datetime.strptime(result[0][0], "%Y-%m-%d").date()
 
-            
-    def getDataIbov(self, start_date = None, end_date = datetime.datetime.now().strftime('%Y-%m-%d')):
-        
+    def getDataIbov(
+        self, start_date=None, end_date=datetime.datetime.now().strftime("%Y-%m-%d")
+    ):
         if not start_date:
-            query = '''
+            query = """
                 SELECT * FROM ibovespa;
-            '''
+            """
         else:
-            query = '''
+            query = """
                 SELECT * FROM ibovespa WHERE date BETWEEN :start_date AND :end_date;
-            '''
+            """
 
         with self.engine.connect() as conn:
-            result = conn.execute(db.text(query), {'start_date': start_date, 'end_date': end_date})
+            result = conn.execute(
+                db.text(query), {"start_date": start_date, "end_date": end_date}
+            )
             data = result.fetchall()
             if data:
-                logger.info('Retornando %s registros do banco de dados [ Start %s | End %s ]', len(data), data[0][0], data[-1][0])
+                logger.info(
+                    "Retornando %s registros do banco de dados [ Start %s | End %s ]",
+                    len(data),
+                    data[0][0],
+                    data[-1][0],
+                )
                 return data
             else:
-                logger.info('Nenhum dado retornado do banco de dados [ Start %s | End %s ]', start_date, end_date)
+                logger.info(
+                    "Nenhum dado retornado do banco de dados [ Start %s | End %s ]",
+                    start_date,
+                    end_date,
+                )
                 return None
 
-            
-    def getDataPred(self, start_date = None, end_date = datetime.datetime.now().strftime('%Y-%m-%d')):
-        
+    def getDataPred(
+        self, start_date=None, end_date=datetime.datetime.now().strftime("%Y-%m-%d")
+    ):
         if not start_date:
-            query = '''
+            query = """
                 SELECT * FROM predict;
-            '''
+            """
         else:
-            query = '''
+            query = """
                 SELECT * FROM predict WHERE date BETWEEN :start_date AND :end_date;
-            '''
+            """
 
         with self.engine.connect() as conn:
-            result = conn.execute(db.text(query), {'start_date': start_date, 'end_date': end_date})
+            result = conn.execute(
+                db.text(query), {"start_date": start_date, "end_date": end_date}
+            )
             data = result.fetchall()
             if data:
-                logger.info('Retornando %s registros de predict [ Start %s | End %s ]', len(data), start_date, end_date)
+                logger.info(
+                    "Retornando %s registros de predict [ Start %s | End %s ]",
+                    len(data),
+                    start_date,
+                    end_date,
+                )
                 return data
             else:
-                logger.info('Nenhum dado retornado de predict [ Start %s | End %s ]', start_date, end_date)
+                logger.info(
+                    "Nenhum dado retornado de predict [ Start %s | End %s ]",
+                    start_date,
+                    end_date,
+                )
                 return None
-                
-    def getDataConsolidate(self, h = 7):
-        
-        query = '''
+
+    def getDataConsolidate(self, h=7):
+        query = """
             SELECT * FROM consolidate ORDER BY date DESC LIMIT :h;
-        '''
+        """
 
         with self.engine.connect() as conn:
-            result = conn.execute(db.text(query), {'h': h})
+            result = conn.execute(db.text(query), {"h": h})
             data = result.fetchall()
             if data:
-                logger.info('Retornando %s registros de predict [ H %s ]', len(data), h)
+                logger.info("Retornando %s registros de predict [ H %s ]", len(data), h)
                 return data
             else:
-                logger.info('Nenhum dado retornado de predict [ H %s ]', h)
+                logger.info("Nenhum dado retornado de predict [ H %s ]", h)
                 return None
 
-            
     def insertIbov(self, data):
-        if data is None: return None
+        if data is None:
+            return None
 
-        query = '''
+        query = """
             INSERT INTO ibovespa 
                 (date, open, high, low, close) 
             VALUES
                 (:date, :open, :high, :low, :close);
-        '''
+        """
         try:
             with self.engine.connect() as conn:
                 if isinstance(data, list):
-                    logger.info('Inserindo %s registros em IBovespa', len(data))
+                    logger.info("Inserindo %s registros em IBovespa", len(data))
                     for ibov in data:
-                        logger.info('Inserindo registro de %s em IBovespa', ibov.date)
-                        conn.execute(db.text(query), {
-                            'date': ibov.date, 
-                            'open': ibov.open, 
-                            'high': ibov.high, 
-                            'low': ibov.low, 
-                            'close': ibov.close
-                        })
+                        logger.info("Inserindo registro de %s em IBovespa", ibov.date)
+                        conn.execute(
+                            db.text(query),
+                            {
+                                "date": ibov.date,
+                                "open": ibov.open,
+                                "high": ibov.high,
+                                "low": ibov.low,
+                                "close": ibov.close,
+                            },
+                        )
                 else:
-                    logger.info('Inserindo registro de %s em IBovespa', data.date)
-                    conn.execute(db.text(query), {
-                        'date': data.date, 
-                        'open': data.open, 
-                        'high': data.high, 
-                        'low': data.low, 
-                        'close': data.close
-                    })
+                    logger.info("Inserindo registro de %s em IBovespa", data.date)
+                    conn.execute(
+                        db.text(query),
+                        {
+                            "date": data.date,
+                            "open": data.open,
+                            "high": data.high,
+                            "low": data.low,
+                            "close": data.close,
+                        },
+                    )
                 conn.commit()
         except Exception as e:
-            logger.error('Erro ao inserir dados na tabela Ibovespa: %s', e)
-            
-    def insertPredict(self, data):
-        if data is None: return None
+            logger.error("Erro ao inserir dados na tabela Ibovespa: %s", e)
 
-        query = '''
+    def insertPredict(self, data):
+        if data is None:
+            return None
+
+        query = """
             INSERT INTO predict 
                 (date, predict) 
             VALUES
                 (:date, :predict);
-        '''
+        """
         try:
             with self.engine.connect() as conn:
                 if isinstance(data, list):
-                    logger.info('Inserindo %s registros em Predict', len(data))
+                    logger.info("Inserindo %s registros em Predict", len(data))
                     for pred in data:
-                        logger.info('Inserindo registro de %s em Predict', pred.date)
-                        conn.execute(db.text(query), {
-                            'date': pred.date, 
-                            'predict': pred.predict,
-                        })
+                        logger.info("Inserindo registro de %s em Predict", pred.date)
+                        conn.execute(
+                            db.text(query),
+                            {
+                                "date": pred.date,
+                                "predict": pred.predict,
+                            },
+                        )
                 else:
-                    logger.info('Inserindo registro de %s em Predict', data.date)
-                    conn.execute(db.text(query), {
-                        'date': pred.date, 
-                        'predict': pred.predict
-                    })
+                    logger.info("Inserindo registro de %s em Predict", data.date)
+                    conn.execute(
+                        db.text(query), {"date": pred.date, "predict": pred.predict}
+                    )
                 conn.commit()
         except Exception as e:
-            logger.error('Erro ao inserir dados  na tabela Predict: %s', e)
-            
+            logger.error("Erro ao inserir dados  na tabela Predict: %s", e)
+
     def insertConsolidate(self, data):
-        if data is None: return None
+        if data is None:
+            return None
 
-        query_exclude = '''
+        query_exclude = """
             DELETE FROM consolidate;
-        '''
+        """
 
-        query = '''
+        query = """
             INSERT INTO consolidate 
                 (date, close, predict, diff, diff_percent) 
             VALUES
                 (:date, :close, :predict, :diff, :diff_percent);
-        '''
+        """
         try:
             with self.engine.connect() as conn:
-                logger.info('Excluindo registros da tabela Consolidate')
+                logger.info("Excluindo registros da tabela Consolidate")
                 conn.execute(db.text(query_exclude))
                 conn.commit()
 
                 if isinstance(data, list):
-                    logger.info('Inserindo %s registros em Consolidate', len(data))
+                    logger.info("Inserindo %s registros em Consolidate", len(data))
                     for consolidate in data:
-                        logger.info('Inserindo registro de %s em Consolidate', consolidate.date)
-                        conn.execute(db.text(query), {
-                            'date': consolidate.date, 
-                            'close': consolidate.close,
-                            'predict': consolidate.predict,
-                            'diff': consolidate.diff,
-                            'diff_percent': consolidate.diff_percent
-                        })
+                        logger.info(
+                            "Inserindo registro de %s em Consolidate", consolidate.date
+                        )
+                        conn.execute(
+                            db.text(query),
+                            {
+                                "date": consolidate.date,
+                                "close": consolidate.close,
+                                "predict": consolidate.predict,
+                                "diff": consolidate.diff,
+                                "diff_percent": consolidate.diff_percent,
+                            },
+                        )
                 else:
-                    logger.info('Inserindo registro de %s em Consolidate', data.date)
-                    conn.execute(db.text(query), {
-                        'date': consolidate.date,
-                        'close': consolidate.close,
-                        'predict': consolidate.predict,
-                        'diff': consolidate.diff,
-                        'diff_percent': consolidate.diff_percent
-                    })
+                    logger.info("Inserindo registro de %s em Consolidate", data.date)
+                    conn.execute(
+                        db.text(query),
+                        {
+                            "date": consolidate.date,
+                            "close": consolidate.close,
+                            "predict": consolidate.predict,
+                            "diff": consolidate.diff,
+                            "diff_percent": consolidate.diff_percent,
+                        },
+                    )
                 conn.commit()
         except Exception as e:
-            logger.error('Erro ao inserir dados na tabela Consolidate: %s', e)
+            logger.error("Erro ao inserir dados na tabela Consolidate: %s", e)
--- tech-challenge/f2-previsao-ibovespa/src/modules/structure.py
+++ tech-challenge/f2-previsao-ibovespa/src/modules/structure.py
@@ -7,23 +7,29 @@ import pandas as pd
 
 from .database import Database
 
-@st.cache_data
+
+@st.cache_data(ttl=1800)
 def getDataIbov(delta=365) -> pd.DataFrame:
-    db = Database(path = os.path.join(os.getenv('DATA_DIR'), 'ibovespa.db'))
-    df = db.getDataIbov(start_date=datetime.datetime.now().date()-datetime.timedelta(days=delta))
+    db = Database(path=os.path.join(os.getenv("DATA_DIR"), "ibovespa.db"))
+    df = db.getDataIbov(
+        start_date=datetime.datetime.now().date() - datetime.timedelta(days=delta)
+    )
+
     return (pd.DataFrame(df), db.getLastDateIbov())
 
-@st.cache_data
+
+@st.cache_data(ttl=1800)
 def getPredict() -> pd.DataFrame:
-    db = Database(path = os.path.join(os.getenv('DATA_DIR'), 'ibovespa.db'))
+    db = Database(path=os.path.join(os.getenv("DATA_DIR"), "ibovespa.db"))
     df = db.getDataPred()
     return (pd.DataFrame(df), db.getLastDatePred())
 
-def getConsolidate(delta=14) -> pd.DataFrame:
-    db = Database(path = os.path.join(os.getenv('DATA_DIR'), 'ibovespa.db'))
-    df = db.getDataConsolidate(h = delta)
-    return pd.DataFrame(df)
 
+@st.cache_data(ttl=1800)
+def getConsolidate(delta=14) -> pd.DataFrame:
+    db = Database(path=os.path.join(os.getenv("DATA_DIR"), "ibovespa.db"))
+    df = db.getDataConsolidate(h=delta)
+    return pd.DataFrame(df)
 
 
 def header() -> None:
@@ -38,33 +44,40 @@ def header() -> None:
     """
     )
 
-def graph_pred(delta = 14, title = "") -> None:
 
+def graph_pred(delta=14, title="") -> None:
     pred = getPredict()[0][-delta:]
-    pred['date'] = pd.to_datetime(pred['date'])
-    pred.set_index('date', inplace=True)
+    pred["date"] = pd.to_datetime(pred["date"])
+    pred.set_index("date", inplace=True)
 
-    ibov = getDataIbov()[0][['date', 'close']]
-    ibov['date'] = pd.to_datetime(ibov['date'])
-    ibov.set_index('date', inplace=True)
-    ibov = ibov.resample('D').ffill().reset_index()
+    ibov = getDataIbov()[0][["date", "close"]]
+    ibov["date"] = pd.to_datetime(ibov["date"])
+    ibov.set_index("date", inplace=True)
+    ibov = ibov.resample("D").ffill().reset_index()
 
-    df = pred.merge(ibov, on='date', how='left')
-    df.rename(columns={'close': 'IBov', 'predict': 'Predição'}, inplace=True)
-    df = df.melt(id_vars=['date'], value_vars=['IBov', 'Predição'])
+    df = pred.merge(ibov, on="date", how="left")
+    df.rename(columns={"close": "IBov", "predict": "Predição"}, inplace=True)
+    df = df.melt(id_vars=["date"], value_vars=["IBov", "Predição"])
 
-    min_close = df['value'].min() * .93
-    max_close = df['value'].max() * 1.03
+    min_close = df["value"].min() * 0.93
+    max_close = df["value"].max() * 1.03
 
-    fig = px.area(df, x='date', y='value', color='variable', title=title, color_discrete_map={'IBov': '#457b9d', 'Predição': '#e63946'})
+    fig = px.area(
+        df,
+        x="date",
+        y="value",
+        color="variable",
+        title=title,
+        color_discrete_map={"IBov": "#457b9d", "Predição": "#e63946"},
+    )
 
     fig.update_xaxes(
         rangeslider_visible=True,
-        rangeslider_bgcolor='white',
+        rangeslider_bgcolor="white",
         rangeslider_thickness=0.18,
     )
 
-    fig.update_traces(stackgroup = None, fill = 'tozeroy')
+    fig.update_traces(stackgroup=None, fill="tozeroy")
 
     fig.update_yaxes(rangemode="normal")
 
@@ -73,87 +86,105 @@ def graph_pred(delta = 14, title = "") -> None:
         yaxis_title="Pontos",
         legend_title="IBovespa",
         yaxis_range=[min_close, max_close],
-        legend_title_text='Índices'
-
+        legend_title_text="Índices",
     )
 
     st.plotly_chart(fig, use_container_width=True)
 
+
 def table_ibovespa() -> None:
-
     pred = getPredict()[0][-90:]
-    pred['date'] = pd.to_datetime(pred['date'])
-    pred.set_index('date', inplace=True)
+    pred["date"] = pd.to_datetime(pred["date"])
+    pred.set_index("date", inplace=True)
 
-    ibov = getDataIbov()[0][['date', 'close']]
-    ibov['date'] = pd.to_datetime(ibov['date'])
-    ibov.set_index('date', inplace=True)
-    ibov = ibov.resample('D').ffill().reset_index()
+    ibov = getDataIbov()[0][["date", "close"]]
+    ibov["date"] = pd.to_datetime(ibov["date"])
+    ibov.set_index("date", inplace=True)
+    ibov = ibov.resample("D").ffill().reset_index()
 
-    df = pred.merge(ibov, on='date', how='left').reset_index(drop=True)
+    df = pred.merge(ibov, on="date", how="left").reset_index(drop=True)
 
-    df_final = df.sort_values(by='date', ascending=False)
-    df_final['date'] = pd.to_datetime(df_final['date']).dt.strftime('%d/%m/%Y')
-    df_final['predict'] = df_final['predict'].round(0).astype(int)
-    df_final['close'] = df_final['close'].fillna(0).astype(int)
-    df_final['diff'] = df_final['close'] - df_final['predict']
-    df_final['diff_percent'] = round((df_final['diff'] / df_final['close']) * 100,1)
-    df_final.set_index('date', inplace=True)
+    df_final = df.sort_values(by="date", ascending=False)
+    df_final["date"] = pd.to_datetime(df_final["date"]).dt.strftime("%d/%m/%Y")
+    df_final["predict"] = df_final["predict"].round(0).astype(int)
+    df_final["close"] = df_final["close"].fillna(0).astype(int)
+    df_final["diff"] = df_final["close"] - df_final["predict"]
+    df_final["diff_percent"] = round((df_final["diff"] / df_final["close"]) * 100, 1)
+    df_final.set_index("date", inplace=True)
 
-    df_final[['close', 'predict', 'diff_percent', 'diff']] = df_final[['close', 'predict', 'diff_percent', 'diff']].astype(str)
+    df_final[["close", "predict", "diff_percent", "diff"]] = df_final[
+        ["close", "predict", "diff_percent", "diff"]
+    ].astype(str)
 
-    df_final['predict'] = df_final['predict'].apply(lambda x: '-' if x == '0' else f"{x} pts.")
-    df_final['close'] = df_final['close'].apply(lambda x: '-' if x == '0' else f"{x} pts.")
-    df_final['diff_percent'] = df_final[['close', 'diff_percent']].apply(lambda x: '-' if x['close'] == '-' else f"{x['diff_percent']}%", axis=1)
-    df_final['diff'] = df_final[['close', 'diff']].apply(lambda x: '-' if x['close'] == '-' else f"{x['diff']} pts.", axis=1)
-    
-    df_final = (
-        df_final
-        .rename(columns={'close': 'Pontuação', 'predict': 'Previsão', 'date': 'Data', 'diff': 'Diferença (pontos)', 'diff_percent': 'Diferença (%)'})
+    df_final["predict"] = df_final["predict"].apply(
+        lambda x: "-" if x == "0" else f"{x} pts."
     )
-    
-    st.dataframe(df_final[['Pontuação', 'Previsão', 'Diferença (pontos)', 'Diferença (%)']], use_container_width=True)
+    df_final["close"] = df_final["close"].apply(
+        lambda x: "-" if x == "0" else f"{x} pts."
+    )
+    df_final["diff_percent"] = df_final[["close", "diff_percent"]].apply(
+        lambda x: "-" if x["close"] == "-" else f"{x['diff_percent']}%", axis=1
+    )
+    df_final["diff"] = df_final[["close", "diff"]].apply(
+        lambda x: "-" if x["close"] == "-" else f"{x['diff']} pts.", axis=1
+    )
+
+    df_final = df_final.rename(
+        columns={
+            "close": "Pontuação",
+            "predict": "Previsão",
+            "date": "Data",
+            "diff": "Diferença (pontos)",
+            "diff_percent": "Diferença (%)",
+        }
+    )
+
+    st.dataframe(
+        df_final[["Pontuação", "Previsão", "Diferença (pontos)", "Diferença (%)"]],
+        use_container_width=True,
+    )
 
 
 def indicators_ibovespa():
-
     last_date = getDataIbov()[1]
 
     data = getConsolidate(30)
-    data = data[data['close'] > 0]
+    data = data[data["close"] > 0]
 
-    mae = round(np.mean(np.abs(data['diff'])))
-    mape = round(np.abs(data['diff'] / data['close']).mean()*100,2)
+    mae = round(np.mean(np.abs(data["diff"])))
+    mape = round(np.abs(data["diff"] / data["close"]).mean() * 100, 2)
 
     cols = st.columns(3)
 
     cols[0].metric(
-        label=":gray[Última atualização IBovespa]", 
-        value=last_date.strftime('%d/%m/%Y')
+        label=":gray[Última atualização IBovespa]", value=last_date.strftime("%d/%m/%Y")
     )
     cols[1].metric(
-        label=f":green[MAE] (Mean Absolute Error) - Últimos 30 Dias", 
-        value=f"± {mae} pts"
+        label=f":green[MAE] (Mean Absolute Error) - Últimos 30 Dias",
+        value=f"± {mae} pts",
     )
     cols[2].metric(
-        label=f":green[MAPE] (Mean Absolute Percent Error) - Últimos 30 Dias", 
-        value= f"{mape}%"
+        label=f":green[MAPE] (Mean Absolute Percent Error) - Últimos 30 Dias",
+        value=f"{mape}%",
     )
 
+
 def desc_project():
+    st.info(
+        "Para ver o conteúdo completo e o código do Dashboard acesse o repositório do projeto em https://github.com/z-fab/postech-data-analytics/tree/master/tech-challenge/f2-previsao-ibovespa",
+        icon="ℹ️",
+    )
+    st.markdown("<br>", unsafe_allow_html=True)
 
-    st.info('Para ver o conteúdo completo e o código do Dashboard acesse o repositório do projeto em https://github.com/z-fab/postech-data-analytics/tree/master/tech-challenge/f2-previsao-ibovespa', icon="ℹ️")
-    st.markdown('<br>', unsafe_allow_html=True)
-
-    content = f'''
+    content = f"""
 
         Este projeto faz parte do módulo de Data Analytics da pós-graduação da POSTECH FIAP, visando desenvolver um modelo preditivo para o fechamento diário do índice IBOVESPA.
 
         O problema fornecido foi: Imagine que você foi escalado para um time de investimentos e precisará realizar um **modelo preditivo** com dados da **IBOVESPA** (Bolsa de valores) para criar uma série temporal e prever diariamente o fechamento da base.
 
-        Dado a característica estocástica do mercado financeiro, a previsão de fechamento da IBOVESPA é um problema _complexo e de difícil solução_, por isso o objetivo será criar um modelo de previsão de fechamento da IBOVESPA apenas para os 3 dias seguintes. A previsão será feita com base nos dados históricos de fechamento.
+        Dado a característica estocástica do mercado financeiro, a previsão de fechamento da IBOVESPA é um problema _complexo e de difícil solução_, por isso o objetivo será criar um modelo de previsão de fechamento da IBOVESPA apenas para o dia seguinte. A previsão será feita com base nos dados históricos de fechamento.
 
-        Como meta, o modelo deve ter um **erro médio percentual absoluto (MAPE) menor que 2%** para os próximos 3 dias.
+        Como meta, o modelo deve ter um **erro médio percentual absoluto (MAPE) menor que 2%** para o próximo dia.
 
         ## Metodologia
 
@@ -177,8 +208,8 @@ def desc_project():
 
         ### Dashboard
 
-        O Dashboard foi criado utilizando o Streamlit. Existe uma rotina que atualiza diariamente os dados e, quando necessário, realiza a previsão para os próximos 3 dias.
+        O Dashboard foi criado utilizando o Streamlit. Existe uma rotina que atualiza diariamente os dados e, quando necessário, realiza a previsão para odia seguinte.
 
-    '''
+    """
 
     st.markdown(content, unsafe_allow_html=True)
