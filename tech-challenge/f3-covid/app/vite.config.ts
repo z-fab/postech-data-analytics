95c386b636a2f64bd8eec79199944b6aef720292
Fabricio Zillig <fabricio.zillig@gmail.com>
2024-03-15 09:41:35 -0300

    Adiciona pasta public

--- tech-challenge/f3-covid/app/vite.config.ts
+++ tech-challenge/f3-covid/app/vite.config.ts
@@ -4,17 +4,11 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/tc-f3",
   plugins: [react()],
   preview: {
-   port: 8080,
-   strictPort: true,
-  },
-  server: {
-   port: 8080,
-   strictPort: true,
-   host: true,
-   origin: "http://0.0.0.0:8080",
+    host: true,
+    port: 8080
   },
   resolve: {
     alias: {
