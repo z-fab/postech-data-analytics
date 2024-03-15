62f6deeb0819645b0293c1aa34f10cac7c9592bc
Fabricio Zillig <fabricio.zillig@gmail.com>
2024-03-14 21:41:56 -0300

    Configuração do Docker

--- tech-challenge/f3-covid/app/vite.config.ts
+++ tech-challenge/f3-covid/app/vite.config.ts
@@ -4,7 +4,18 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview: {
+   port: 8080,
+   strictPort: true,
+  },
+  server: {
+   port: 8080,
+   strictPort: true,
+   host: true,
+   origin: "http://0.0.0.0:8080",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
